510aa9876a1ee01670f137aaf3651e498e5d1cec
shahdelaref <selaref919@gmail.com>
2025-07-27 16:15:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
-// عناصر الإدخال
+
 let siteNameInput = document.getElementById('siteName');
 let siteUrlInput = document.getElementById('siteUrl');
 let submitBtn = document.getElementById('submitBtn');
 let tableBody = document.getElementById('tableBody');
 let msg = document.getElementById('msg');
 
-// مصفوفة لحفظ المواقع
 let sites = [];
 
-// تحميل المواقع من localStorage إذا وجدت
+
 if (localStorage.getItem('sites')) {
   sites = JSON.parse(localStorage.getItem('sites'));
   displaySites();
 }
 
-// التحقق من صحة البيانات
 function validateSite(name, url) {
   let nameValid = name.length >= 3;
   let urlValid = /^(https?:\/\/)?([\w\-]+\.)+[\w\-]+(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)?$/.test(url);
   return nameValid && urlValid;
 }
-
-// إضافة موقع جديد
 submitBtn.onclick = function () {
   let name = siteNameInput.value.trim();
   let url = siteUrlInput.value.trim();
@@ -38,8 +34,6 @@ submitBtn.onclick = function () {
   displaySites();
   clearForm();
 };
-
-// عرض المواقع في الجدول
 function displaySites() {
   let html = '';
   for (let i = 0; i < sites.length; i++) {
@@ -54,8 +48,6 @@ function displaySites() {
   }
   tableBody.innerHTML = html;
 }
-
-// حذف موقع
 window.deleteSite = function(index) {
   sites.splice(index, 1);
   localStorage.setItem('sites', JSON.stringify(sites));
